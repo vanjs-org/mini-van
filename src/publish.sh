3a2290941dd67d04cd8108ac23f0b858cabf7c5e
Tao Xin <tao@vanjs.org>
2023-06-19 14:32:05 -0700

    Add .min.gz size info for Mini-Van

--- src/publish.sh
+++ src/publish.sh
@@ -9,6 +9,7 @@ cp mini-van.d.ts ../public/mini-van-$VER.d.ts
 ../node_modules/esbuild/bin/esbuild mini-van.forbundle.js --bundle --outfile=../public/mini-van-$VER.nomodule.js
 
 $TERSER mini-van.js --compress --toplevel --mangle --mangle-props keep_quoted -f wrap_func_args=false -o ../public/mini-van-$VER.min.js
+gzip -kf ../public/mini-van-$VER.min.js
 cp mini-van.d.ts ../public/mini-van-$VER.min.d.ts
 MIN_NOMODULE=$($TERSER ../public/mini-van-$VER.nomodule.js --compress --toplevel --mangle --mangle-props keep_quoted -f wrap_func_args=false)
 echo -n "{let${MIN_NOMODULE:3}}" > ../public/mini-van-$VER.nomodule.min.js
@@ -17,6 +18,7 @@ echo -n "{let${MIN_NOMODULE:3}}" > ../public/mini-van-$VER.nomodule.min.js
 cp ../public/mini-van-$VER.js ../public/mini-van-latest.js
 cp ../public/mini-van-$VER.d.ts ../public/mini-van-latest.d.ts
 cp ../public/mini-van-$VER.min.js ../public/mini-van-latest.min.js
+cp ../public/mini-van-$VER.min.js.gz ../public/mini-van-latest.min.js.gz
 cp ../public/mini-van-$VER.min.d.ts ../public/mini-van-latest.min.d.ts
 cp ../public/mini-van-$VER.nomodule.js ../public/mini-van-latest.nomodule.js
 cp ../public/mini-van-$VER.nomodule.min.js ../public/mini-van-latest.nomodule.min.js
