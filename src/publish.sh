e51fb3a42a9b5ef90f384ff613fc0af8a92f8ae7
Tao Xin <tao@vanjs.org>
2023-08-09 21:30:17 -0700

    Use npx to run dev binaries

--- src/publish.sh
+++ src/publish.sh
@@ -1,17 +1,17 @@
 set -e
 
+# node-jq doesn't export a binary in node_modules/.bin/, thus we're using the fully qualified path here
 VER=$(../node_modules/node-jq/bin/jq -r '.version | split("-")[0]' ../package.json)
 echo -n $VER > ../public/mini-van.version
-TERSER=../node_modules/terser/bin/terser
 
 cp mini-van.js ../public/mini-van-$VER.js
 cp mini-van.d.ts ../public/mini-van-$VER.d.ts
-../node_modules/esbuild/bin/esbuild mini-van.forbundle.js --bundle --outfile=../public/mini-van-$VER.nomodule.js
+npx esbuild mini-van.forbundle.js --bundle --outfile=../public/mini-van-$VER.nomodule.js
 
-$TERSER mini-van.js --compress --toplevel --mangle --mangle-props keep_quoted -f wrap_func_args=false -o ../public/mini-van-$VER.min.js
+npx terser mini-van.js --compress --toplevel --mangle --mangle-props keep_quoted -f wrap_func_args=false -o ../public/mini-van-$VER.min.js
 gzip -kf ../public/mini-van-$VER.min.js
 cp mini-van.d.ts ../public/mini-van-$VER.min.d.ts
-MIN_NOMODULE=$($TERSER ../public/mini-van-$VER.nomodule.js --compress --toplevel --mangle --mangle-props keep_quoted -f wrap_func_args=false)
+MIN_NOMODULE=$(npx terser ../public/mini-van-$VER.nomodule.js --compress --toplevel --mangle --mangle-props keep_quoted -f wrap_func_args=false)
 echo -n "{let${MIN_NOMODULE:3}}" > ../public/mini-van-$VER.nomodule.min.js
 # echo -n "$MIN_NOMODULE" > ../public/mini-van-$VER.nomodule.min.js
 
@@ -24,5 +24,5 @@ cp ../public/mini-van-$VER.nomodule.js ../public/mini-van-latest.nomodule.js
 cp ../public/mini-van-$VER.nomodule.min.js ../public/mini-van-latest.nomodule.min.js
 
 # Testing
-../node_modules/typescript/bin/tsc -m es2020 -t es2017 ../test/mini-van.test.ts
-../node_modules/esbuild/bin/esbuild ../test/mini-van.test.forbundle.js --bundle --outfile=../test/mini-van.test.nomodule.js
+npx tsc -m es2020 -t es2017 ../test/mini-van.test.ts
+npx esbuild ../test/mini-van.test.forbundle.js --bundle --outfile=../test/mini-van.test.nomodule.js
